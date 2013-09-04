d7c29e49c7667db10ab8bd950af4ccb5840f6387
Cameron Bytheway <cameron@theflokk.com>
2013-09-04 11:18:25 -0600

    finish track handler

--- src/pivot_api_track_handler.erl
+++ src/pivot_api_track_handler.erl
@@ -7,38 +7,41 @@
 -export([handle/2]).
 -export([terminate/3]).
 
+-define(ERROR (Error, Req), cowboy_req:reply(400, [{<<"content-type">>, <<"application/json">>}], <<"{\"error\":{\"message\":\"", Error/binary, "\",\"code\":400}}">>, Req)).
+
 init(_Transport, Req, []) ->
   {ok, Req, undefined}.
 
 handle(Req, State) ->
   {Method, Req2} = cowboy_req:method(Req),
+  {ok, Req3} = check_method(Method, Req2),
+  {ok, Req3, State}.
+
+check_method(<<"GET">>, Req) ->
+  {App, Req2} = cowboy_req:qs_val(<<"a">>, Req),
   {Event, Req3} = cowboy_req:qs_val(<<"e">>, Req2),
-  {ID, Req4} = cowboy_req:qs_val(<<"i">>, Req3),
-  {Test, Req5} = cowboy_req:qs_val(<<"t">>, Req4),
-
-  {ok, Req6} = respond(Method, Event, ID, Test, Req5),
-  {ok, Req6, State}.
-
-respond(<<"GET">>, undefined, _, _, Req) ->
-  cowboy_req:reply(400, [], <<"Missing 'e' parameter.">>, Req);
-respond(<<"GET">>, _, undefined, _, Req) ->
-  cowboy_req:reply(400, [], <<"Missing 'i' parameter.">>, Req);
-respond(<<"GET">>, Event, ID, Test, Req) ->
-  Env = case Test of
-    <<"1">> ->
-      test;
-    _ ->
-      prod
-  end,
-
-  {Ref, Req} = cowboy_req:meta(pivot_ref, Req),
-  % pivot:event(Ref, Env, Event, ID),
-  io:format("Ref ~p Event ~p ID ~p Env ~p\n", [Ref, Event, ID, Env]),
-
-  cowboy_req:reply(200, [], <<"">>, Req);
-respond(_, _, _, _, Req) ->
+  {UserID, Req4} = cowboy_req:qs_val(<<"u">>, Req3),
+  maybe_track(App, Event, UserID, Req4);
+check_method(<<"POST">>, Req) ->
+  {ok, Params, Req2} = cowboy_req:body_qs(Req),
+  App = fast_key:get(<<"a">>, Params),
+  Event = fast_key:get(<<"e">>, Params),
+  UserID = fast_key:get(<<"u">>, Params),
+  maybe_track(App, Event, UserID, Req2);
+check_method(_, Req) ->
   %% Method not allowed.
   cowboy_req:reply(405, Req).
+
+maybe_track(undefined, _, _, Req) ->
+  ?ERROR(<<"Missing app (a) parameter.">>, Req);
+maybe_track(_, undefined, _, Req) ->
+  ?ERROR(<<"Missing event (e) parameter.">>, Req);
+maybe_track(_, _, undefined, Req) ->
+  ?ERROR(<<"Missing user id (u) parameter.">>, Req);
+maybe_track(App, Event, UserID, Req) ->
+  {Ref, Req} = cowboy_req:meta(pivot_ref, Req),
+  ok = pivot:track(Ref, App, Event, UserID),
+  cowboy_req:reply(200, [], <<"">>, Req).
 
 terminate(_Reason, _Req, _State) ->
   ok.
